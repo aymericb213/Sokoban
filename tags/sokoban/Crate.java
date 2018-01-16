8a775c919ce8034f6184c72135945190e587f27c
Leblond Valentin <21609038@n304l-401018.etu.info.unicaen.fr>
2018-01-16 17:00:46 +0100

    début board

--- tags/sokoban/Crate.java
+++ tags/sokoban/Crate.java
@@ -3,6 +3,9 @@ package sokoban;
 
 class Crate extends Block {
 
+  private boolean deadLock;
+  private boolean isPlaced;
+
   public Crate (int x, int y) {
     super(x,y);
   }
--- tags/sokoban/Player.java
+++ tags/sokoban/Player.java
@@ -3,6 +3,8 @@ package sokoban;
 
 class Player extends Block {
 
+  private boolean onObjective;
+
   public Player(int x, int y) {
     super(x,y);
   }
