c9814f78f6af43fd28646a865cf19d6643ab90d1
Mori Baptiste <21602052@n304l-159015.etu.info.unicaen.fr>
2018-01-31 13:55:29 +0100

    player.move sans optimisations

--- tags/sokoban/Player.java
+++ tags/sokoban/Player.java
@@ -30,9 +30,24 @@ public class Player extends Block {
     if (new_place instanceof Objective){
 
       System.out.println("objectif");
+
       b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
-      Block t = new FreeTile(this.x,this.y);
-      b.grid[this.x][this.y]=t;
+
+      Block temp = new FreeTile(this.x,this.y);
+
+      if (onObjective){
+        this.onObjective=false;
+        Block obje = new Objective(this.x,this.y);
+        for (Block obj : b.listObjective){
+          if (obj.x==this.x && obj.y==this.y){
+            obje=obj;
+            break;
+          }
+        }
+        temp=obje;
+      }
+
+      b.grid[this.x][this.y]=temp;
 
       this.x+=position.get(0);
       this.y+=position.get(1);
@@ -40,65 +55,112 @@ public class Player extends Block {
 
 
     } else if (new_place instanceof Crate && !(((Crate)new_place).isBlocked())){
+
+
+
+      if (((Crate)new_place).isPlaced()){
+
         if (b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Objective){
 
-          b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))]=b.grid[this.x+position.get(0)][this.y+position.get(1)];
+          ((Crate)new_place).setPlaced(true);
 
-          ((Crate)b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))]).setPlaced(true);
+        } else if (b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof FreeTile) {
 
-          b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
-          Block t = new FreeTile(this.x,this.y);
-          b.grid[this.x][this.y]=t;
+          ((Crate)new_place).setPlaced(false);
 
-          this.x+=position.get(0);
-          this.y+=position.get(1);
-
-        } else if (!(b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Wall || b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Crate)){
-
-          b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))]=b.grid[this.x+position.get(0)][this.y+position.get(1)];
-
-          b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
-          Block t = new FreeTile(this.x,this.y);
-          b.grid[this.x][this.y]=t;
-
-          this.x+=position.get(0);
-          this.y+=position.get(1);
         }
 
-        /* rajouter une condition si la caisse est sur un objectif */
+        if (b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Objective || b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof FreeTile){
+
+          b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))]=b.grid[this.x+position.get(0)][this.y+position.get(1)];
+
+          b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
+
+
+          Block temp = new FreeTile(this.x,this.y);
+
+          if (onObjective){
+            this.onObjective=false;
+            Block obje = new Objective(this.x,this.y);
+            for (Block obj : b.listObjective){
+              if (obj.x==this.x && obj.y==this.y){
+                obje=obj;
+                break;
+              }
+            }
+            temp=obje;
+          }
+
+          b.grid[this.x][this.y]=temp;
+
+          this.x+=position.get(0);
+          this.y+=position.get(1);
+          this.onObjective=true;
+        }
+
+
+      } else {
+
+        if (b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Objective){
+
+          ((Crate)new_place).setPlaced(true);
+
+        }
+
+        if (b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof Objective || b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))] instanceof FreeTile){
+
+          b.grid[this.x+(2*position.get(0))][this.y+(2*position.get(1))]=b.grid[this.x+position.get(0)][this.y+position.get(1)];
+
+          b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
+
+
+          Block temp = new FreeTile(this.x,this.y);
+
+          if (onObjective){
+            this.onObjective=false;
+            Block obje = new Objective(this.x,this.y);
+            for (Block obj : b.listObjective){
+              if (obj.x==this.x && obj.y==this.y){
+                obje=obj;
+                break;
+              }
+            }
+            temp=obje;
+          }
+
+          b.grid[this.x][this.y]=temp;
+
+          this.x+=position.get(0);
+          this.y+=position.get(1);
+
+        }
+
+      }
 
 
 
-
-    } else if (!( new_place instanceof Wall)){
+    } else if (new_place instanceof FreeTile){
 
       Block temp = new FreeTile(this.x,this.y);
+
+      if (onObjective){
+        this.onObjective=false;
+        Block obje = new Objective(this.x,this.y);
+        for (Block obj : b.listObjective){
+          if (obj.x==this.x && obj.y==this.y){
+            obje=obj;
+            break;
+          }
+        }
+        temp=obje;
+      }
+
       b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
       b.grid[this.x][this.y]=temp;
 
       this.x+=position.get(0);
       this.y+=position.get(1);
     }
-
-
-    if (onObjective==true){
-
-      Block obje = new Objective(this.x,this.y);
-      System.out.println("remet lobj");
-      for (Block obj : b.listObjective){
-        if (obj.x==this.x && obj.y==this.y){
-          obje=obj;
-          break;
-        }
-      }
-      this.onObjective=false;
-      b.grid[this.x+position.get(0)][this.y+position.get(1)]=b.grid[this.x][this.y];
-      b.grid[this.x][this.y]=obje;
-      this.x+=position.get(0);
-      this.y+=position.get(1);
-      this.onObjective=false;
-    }
-
 
     return b;
   }
