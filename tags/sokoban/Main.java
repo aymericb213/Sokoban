40dd7dece14731aa5af948ef0b28afc7edc53947
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-02-02 15:39:48 +0100

    .xsb en argument de Main

--- tags/sokoban/Main.java
+++ tags/sokoban/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		System.out.print("\033[H\033[2J");
     Board b= new Board();
 		Scanner sc= new Scanner(System.in);
-    MapReader map = new MapReader("sokoban/maps/"+args[0]);
+    MapReader map = new MapReader("sokoban/maps/" + args[0]);
     map.readingMap();
     b.createGrid(map.getMap());
 		while (!b.isFinished()) {
