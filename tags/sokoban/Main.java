8bbd0796d699465fea6ddfc7d95a791c5e0eae92
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-02-02 14:19:49 +0100

    Map supplémentaire et amélioratio interface console

--- tags/sokoban/Main.java
+++ tags/sokoban/Main.java
@@ -9,30 +9,32 @@ public class Main {
   public static void main (String[] args) throws IOException {
     /*ArrayList<ArrayList> map = Board.readingMap("sokoban/maps/map1.xsb");
     System.out.println(map);*/
-
+		System.out.print("\033[H\033[2J");
     Board b= new Board();
 		Scanner sc= new Scanner(System.in);
     MapReader map = new MapReader("sokoban/maps/map1.xsb");
     map.readingMap();
     b.createGrid(map.getMap());
 		while (!b.isFinished()) {
-			System.out.println(b.toString());
+			System.out.println("================ SOKOBAN =================\n");
+			System.out.print("Niveau 1\n");//enlever codage en dur
+			System.out.println("\n" + b.toString());
 			System.out.println("Entrez votre prochain mouvement parmi H (haut), G (gauche), B (bas), D (droite) :");
 			String input=sc.nextLine();
 			ArrayList<Integer> nextMove = new ArrayList<>();
-			if (input.equals("H")) {
+			if (input.equals("H") || input.equals("h")) {
 				nextMove.add(-1);
 				nextMove.add(0);
 			}
-			else if (input.equals("G")) {
+			else if (input.equals("G") || input.equals("g")) {
 				nextMove.add(0);
 				nextMove.add(-1);
 			}
-			else if (input.equals("D")) {
+			else if (input.equals("D") || input.equals("d")) {
 				nextMove.add(0);
 				nextMove.add(1);
 			}
-			else if (input.equals("B")) {
+			else if (input.equals("B") || input.equals("b")) {
 				nextMove.add(1);
 				nextMove.add(0);
 			}
@@ -42,12 +44,13 @@ public class Main {
 				System.out.println("Les seules commandes valides sont H,G,D,B.");
 			}
 			((Player)b.player).move(b, nextMove);
+			System.out.print("\033[H\033[2J");
 		}
     System.out.println(b.toString());
 		boolean win = true;
 		for(Block c : b.listCrate) {
 			if (((Crate)c).deadLock){
-				System.out.println("Game Over, you're so bad, you lose");
+				System.out.println("Game Over (plus de mouvement possible)");
 				win=false;
 				break;
 			}
