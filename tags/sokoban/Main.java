1bbe1a52f75f684647a94bfc08576c2d986a10c5
Chagneux Dimitri <21606807@n304l-401018.etu.info.unicaen.fr>
2018-02-06 16:19:02 +0100

    chargement d'une nouvelle map

--- tags/sokoban/Main.java
+++ tags/sokoban/Main.java
@@ -16,76 +16,97 @@ public class Main {
 	* et le met à jour en fonction des entrées utilisateur.
 	* On peut exécuter la classe de trois façons différentes :
 	* - si aucun argument n'est donné, on parcourt les niveaux dans l'ordre;
-	* - si on donne en argument un nom de fichier .xsb présent dans le dossier maps, le niveau sélectionné sera affiché;
-	* - si l'argument est -r, un niveau est choisi au hasard.  
+	* - si on donne en argument un numéro de map présente dans le dossier maps, le niveau sélectionné sera affiché;
+	* - si l'argument est -r, un niveau est choisi au hasard.
 	*/
   public static void main (String[] args) throws IOException {
 		System.out.println("\033[H\033[2J");
 		int nbMaps = new File("sokoban/maps").list().length;
 		int nbMoves = 0;
+    String gContinue = "O";
     Board b= new Board();
 		Scanner sc= new Scanner(System.in);
 		MapReader map = new MapReader("");
+    Random r = new Random();
 		if (args.length>0) {
 			if (args[0].equals("-r")) {
-				Random r = new Random();
 				int n = r.nextInt(nbMaps)+1;
 				map.setFile("sokoban/maps/map" + n + ".xsb");
 			} else {
-				map.setFile("sokoban/maps/" + args[0]);
+				map.setFile("sokoban/maps/map" + args[0] + ".xsb");
 			}
 		} else {
 			map.setFile("sokoban/maps/map1.xsb");
 		}
-		map.readingMap();
-    b.createGrid(map.getMap());
-		while (!b.isFinished()) {
-			System.out.println("================ SOKOBAN =================\n");
-			System.out.println("Niveau " + map.getFile().charAt(map.getFile().length() - 5)+"\n");
-			System.out.println("\n" + b.toString());
-			System.out.println("# : mur");
-			System.out.println(". : objectif");
-			System.out.println("$ : caisse (* si placée sur un objectif)");
-			System.out.println("@ : joueur (+ si placé sur un objectif)");
-			System.out.println("\nEntrez votre prochain mouvement parmi H (haut), G (gauche), B (bas), D (droite) :");
-			String input=sc.nextLine();
-			ArrayList<Integer> nextMove = new ArrayList<>();
-			if (input.equals("H") || input.equals("h")) {
-				nextMove.add(-1);
-				nextMove.add(0);
-			}
-			else if (input.equals("G") || input.equals("g")) {
-				nextMove.add(0);
-				nextMove.add(-1);
-			}
-			else if (input.equals("D") || input.equals("d")) {
-				nextMove.add(0);
-				nextMove.add(1);
-			}
-			else if (input.equals("B") || input.equals("b")) {
-				nextMove.add(1);
-				nextMove.add(0);
-			}
-			else {
-				nextMove.add(0);
-				nextMove.add(0);
-				System.out.println("Les seules commandes valides sont H,G,D,B.");
-			}
-			((Player)b.player).move(b, nextMove);
-			nbMoves+=1;
-			System.out.println("\033[H\033[2J");
-		}
-    System.out.println(b.toString());
-		boolean win = true;
-		for(Block c : b.listCrate) {
-			if (((Crate)c).deadLock){
-				System.out.println("Game Over (plus de mouvement possible)");
-				win=false;
-				break;
-			}
-		}
-		if (win){
-			System.out.println("Niveau terminé en " + nbMoves + " déplacements");
-		}
-  }
+    while (gContinue.equals("O")) {
+      map.readingMap();
+      b.createGrid(map.getMap());
+  		while (!b.isFinished()) {
+  			System.out.println("================ SOKOBAN =================\n");
+  			System.out.println("Niveau " + map.getFile().charAt(map.getFile().length() - 5)+"\n");
+  			System.out.println("\n" + b.toString());
+  			System.out.println("# : mur");
+  			System.out.println(". : objectif");
+  			System.out.println("$ : caisse (* si placée sur un objectif)");
+  			System.out.println("@ : joueur (+ si placé sur un objectif)");
+  			System.out.println("\nEntrez votre prochain mouvement parmi H (haut), G (gauche), B (bas), D (droite) :");
+  			String input=sc.nextLine();
+  			ArrayList<Integer> nextMove = new ArrayList<>();
+  			if (input.equals("H") || input.equals("h")) {
+  				nextMove.add(-1);
+  				nextMove.add(0);
+  			}
+  			else if (input.equals("G") || input.equals("g")) {
+  				nextMove.add(0);
+  				nextMove.add(-1);
+  			}
+  			else if (input.equals("D") || input.equals("d")) {
+  				nextMove.add(0);
+  				nextMove.add(1);
+  			}
+  			else if (input.equals("B") || input.equals("b")) {
+  				nextMove.add(1);
+  				nextMove.add(0);
+  			}
+  			else {
+  				nextMove.add(0);
+  				nextMove.add(0);
+  				System.out.println("Les seules commandes valides sont H,G,D,B.");
+  			}
+  			((Player)b.player).move(b, nextMove);
+  			nbMoves+=1;
+  			System.out.println("\033[H\033[2J");
+  		}
+      System.out.println(b.toString());
+  		boolean win = true;
+  		for(Block c : b.listCrate) {
+  			if (((Crate)c).deadLock){
+  				System.out.println("Game Over (plus de mouvement possible)");
+  				win=false;
+  				break;
+  			}
+  		}
+  		if (win){
+  			System.out.println("Niveau terminé en " + nbMoves + " déplacements");
+        nbMoves = 0;
+        System.out.println("Lancer le prochain niveau ? (O/N)");
+        String input2 = sc.nextLine();
+        if (input2.equals("O") || input2.equals("o")) {
+          if (args[0].equals("-r")) {
+    				int temp = r.nextInt(nbMaps)+1;
+          } else {
+            int temp = Integer.parseInt(args[0]);
+            temp += 1;
+            if (temp > nbMaps) {
+              System.out.println("Il n'y a plus de map disponible");
+            } else {
+              map.setFile("sokoban/maps/map" + temp + ".xsb");
+            }
+          }
+        }
+        gContinue = input2;
+  		}
+    }
+    }
+
 }
