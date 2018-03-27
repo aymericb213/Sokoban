c131ecc989580559609b9bdad616c68fad0c6dd3
Leblond Valentin <21609038@n304l-401015.etu.info.unicaen.fr>
2018-03-27 17:23:46 +0200

    doc main de graphique

--- tags/graphique/Main.java
+++ tags/graphique/Main.java
@@ -9,8 +9,9 @@ import java.io.*;
 public class Main {
     /**
       * Lance la fenetre de Login
+      * @param args liste arguments nom utilisees
       */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
       new Login();
     }
 
