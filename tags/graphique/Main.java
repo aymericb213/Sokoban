7777981f683a5e8c44797835b903487d7282093f
Leblond Valentin <21609038@n304l-401015.etu.info.unicaen.fr>
2018-03-27 17:16:48 +0200

    doc main de graphique

--- tags/graphique/Main.java
+++ tags/graphique/Main.java
@@ -3,8 +3,13 @@ package graphique;
 import sokoban.*;
 import java.io.*;
 
+/**
+  * Classe exécutable du package graphique
+  */
 public class Main {
-
+    /**
+      * Lance la fenetre de Login
+      */
     public static void main(String[] args) throws IOException {
       new Login();
     }
